17991eb1c0164efcc5a94d602bdfdf928cea58d8
Collin Poczatek <collin.poczatek@gmail.com>
2022-12-12 09:54:26 -0800

    add bllock attachments

--- instances/compute.tf
+++ instances/compute.tf
@@ -42,7 +42,10 @@ locals {
   instance.id => instance }
   num_instances    = length(local.old_instances)
   new_boot_volumes = local.state_file.outputs.new_boot_volumes.value[0]
+  new_volumes      = local.state_file.outputs.new_volumes.value[0]
+  #new_volume_by_old_instance =
   old_vnics        = local.state_file.outputs.existing_vnics.value
+  old_attachments  = local.state_file.outputs.existing_block_volume_attachments.value.volume_attachments
 }
 
 ###
@@ -69,8 +72,8 @@ resource "oci_core_instance" "new_instances" {
     display_name           = local.old_vnics[each.key].display_name
     assign_public_ip       = (local.old_vnics[each.key].public_ip_address == "" ? false : true)
     nsg_ids                = local.old_vnics[each.key].nsg_ids
-    private_ip             = local.old_vnics[each.key].private_ip_address
-    hostname_label         = local.old_vnics[each.key].hostname_label
+    #private_ip             = local.old_vnics[each.key].private_ip_address
+    #hostname_label         = local.old_vnics[each.key].hostname_label
     skip_source_dest_check = false
   }
 
@@ -85,15 +88,32 @@ resource "oci_core_instance" "new_instances" {
   freeform_tags = { "old_instance" = each.key }
 }
 
+###
+# New attachments
+###
+
+resource "oci_core_volume_attachment" "test_volume_attachment" {
+  for_each = {
+    for attachment in local.old_attachments :
+  attachment.id => attachment }
+
+  attachment_type = each.value.attachment_type
+  instance_id     = oci_core_instance.new_instances[each.value.instance_id].id
+  volume_id       = local.new_volumes[each.key].id
+  #depends_on = [oci_core_instance.new_instances]
+}
 
 ###
 # Outputs
 ###
 
-output "old_instances" {
-  value = local.old_instances
-}
+#output "old_instances" {
+#  value = local.old_instances
+#}
 
+output "new_instances" {
+  value = oci_core_instance.new_instances
+}
 #output "state_file" {
 #  value = local.state_file
 #}
